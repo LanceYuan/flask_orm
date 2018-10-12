55c8cd8cbb36fe912378ecea9d77dc260f1045ed
Lance <yuanshunchang@qhchjf.com>
2018-10-12 16:51:24 +0800

    flask_migrate  数据库管理.

--- manage.py
+++ manage.py
@@ -1,8 +1,15 @@
 from app import create_app
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from app import db
 
 app = create_app()
 manager = Manager(app)
+Migrate(app, db)
+
+# flask-migrate 依懒于flask-script
+# 增加数据初始化相关操作 python manage.py db init|migrate|upgrade
+manager.add_command("db", MigrateCommand)
 
 # python manage.py cmd lance 给manager增加参数.
 @manager.command
