11bd6e7e13fe84d5f3c06ce41c4c6cf8c4c2a17b
Lance <yuanshunchang@qhchjf.com>
2018-10-12 14:39:39 +0800

    flask_script 通过脚本runserver启动程序.

--- manage.py
+++ manage.py
@@ -1,8 +1,13 @@
 from app import create_app
-
+from flask_script import Manager
 
 app = create_app()
+manager = Manager(app)
 
+# python manage.py cmd lance 给manager增加参数.
+@manager.command
+def cmd(args):
+    print(args)
 
 if __name__ == "__main__":
-    app.run()
+    manager.run()
